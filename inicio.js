5aa85c14ce3a6f057c299c0664febcd0e52f1462
Adrian <a.pinzon@correo.unimet.edu.ve>
2025-02-16 00:04:01 -0400

    Create inicio.js

---
+++ inicio.js
@@ -0,0 +1,26 @@
+'use strict';
+document.getElementById("maxScore").textContent = "Max: " + maxScore
+let jugar = document.getElementById("jugar");
+let nombre = document.getElementById("nombre");
+let puntuaciones = document.getElementById("puntuaciones");
+
+jugar.onclick = function() {
+    if (nombre.value != "") {
+        localStorage.setItem("jugadorActual", nombre.value);
+        console.log(localStorage.getItem("jugadorActual"));
+        const usuarioGuardado = localStorage.getItem(nombre.value);
+        if (usuarioGuardado === null) {
+            localStorage.setItem(nombre.value, 0)
+            alert("nuevo usuario")
+            window.location="./try.html"
+        }
+        else {
+            window.location="./try.html"    
+        }
+    }
+
+}
+
+puntuaciones.onclick = function () {
+    window.location = "./record.html"
+}
